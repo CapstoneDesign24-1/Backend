ac04265b85b785dadcae2c2fb1e241215c3c2b40
zestbear <zestbear90@gmail.com>
2024-04-01 23:30:45 +0900

    feat: 사용자 repository 추가

---
+++ src/main/java/com/boj/guidance/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.boj.guidance.repository;
+
+import com.boj.guidance.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, UUID> {
+}
