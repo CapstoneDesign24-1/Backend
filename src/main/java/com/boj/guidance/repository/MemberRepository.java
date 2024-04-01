e38688704427d81c14a9bf301f3972fc8352523a
zestbear <zestbear90@gmail.com>
2024-04-01 23:34:23 +0900

    feat: 사용자 로그인 위한 검색 쿼리 추가

--- src/main/java/com/boj/guidance/repository/MemberRepository.java
+++ src/main/java/com/boj/guidance/repository/MemberRepository.java
@@ -11,4 +11,10 @@ import java.util.UUID;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, UUID> {
+
+    @Query("select m " +
+            "from Member m " +
+            "where m.login_id = :id and m.login_password = :password")
+    Optional<Member> findMemberByLogin_idAndLogin_password(@Param("id") String login_id, @Param("password") String login_password);
+
 }
