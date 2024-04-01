3272da414cf2e9fe0e227df5de0f519dd4876dd9
zestbear <zestbear90@gmail.com>
2024-04-01 23:29:10 +0900

    feat: jpa config 추가

---
+++ src/main/java/com/boj/guidance/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.boj.guidance.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+    public JpaConfig() {
+    }
+}
