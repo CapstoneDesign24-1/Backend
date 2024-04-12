d70862fc36bb65fbd9a43f4c8f80974f0c209bb9
zestbear <zestbear90@gmail.com>
2024-04-12 15:57:57 +0900

    feat: 사용자 tier 치환

---
+++ src/main/java/com/boj/guidance/util/MemberTierUtil.java
@@ -0,0 +1,73 @@
+package com.boj.guidance.util;
+
+public class MemberTierUtil {
+
+    public static String checkTier(Long tier) {
+        if (tier == 1) {
+            return "Bronze 5";
+        } else if (tier == 2) {
+            return "Bronze 4";
+        } else if (tier == 3) {
+            return "Bronze 3";
+        } else if (tier == 4) {
+            return "Bronze 2";
+        } else if (tier == 5) {
+            return "Bronze 1";
+        } else if (tier == 6) {
+            return "Silver 5";
+        } else if (tier == 7) {
+            return "Silver 4";
+        } else if (tier == 8) {
+            return "Silver 3";
+        } else if (tier == 9) {
+            return "Silver 2";
+        } else if (tier == 10) {
+            return "Silver 1";
+        } else if (tier == 11) {
+            return "Gold 5";
+        } else if (tier == 12) {
+            return "Gold 4";
+        } else if (tier == 13) {
+            return "Gold 3";
+        } else if (tier == 14) {
+            return "Gold 2";
+        } else if (tier == 15) {
+            return "Gold 1";
+        } else if (tier == 16) {
+            return "Platinum 5";
+        } else if (tier == 17) {
+            return "Platinum 4";
+        } else if (tier == 18) {
+            return "Platinum 3";
+        } else if (tier == 19) {
+            return "Platinum 2";
+        } else if (tier == 20) {
+            return "Platinum 1";
+        } else if (tier == 21) {
+            return "Diamond 5";
+        } else if (tier == 22) {
+            return "Diamond 4";
+        } else if (tier == 23) {
+            return "Diamond 3";
+        } else if (tier == 24) {
+            return "Diamond 2";
+        } else if (tier == 25) {
+            return "Diamond 1";
+        } else if (tier == 26) {
+            return "Ruby 5";
+        } else if (tier == 27) {
+            return "Ruby 4";
+        } else if (tier == 28) {
+            return "Ruby 3";
+        } else if (tier == 29) {
+            return "Ruby 2";
+        } else if (tier == 30) {
+            return "Ruby 1";
+        } else if (tier == 31) {
+            return "Master";
+        } else {
+            return "None";
+        }
+    }
+
+}
