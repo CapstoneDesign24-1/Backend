5ebd946a7a5f1e93c907fc7420841006fed99337
zestbear <zestbear90@gmail.com>
2024-04-12 23:02:29 +0900

    feat: 비밀번호 암호화 예외처리

--- src/main/java/com/boj/guidance/config/PasswordEncoder.java
+++ src/main/java/com/boj/guidance/config/PasswordEncoder.java
@@ -1,45 +1,40 @@
 package com.boj.guidance.config;
 
+import com.boj.guidance.util.api.ResponseCode;
+import com.boj.guidance.util.exception.EncodingException;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 @Component
 @NoArgsConstructor
 public class PasswordEncoder {
 
-    private final String ALGORITHM = "AES";
     @Value("${password.encoder}")
     private String KEY;
 
-    public String encrypt(String data) throws Exception {
+    public String encrypt(String data) {
         try {
+            String ALGORITHM = "AES";
             Key key = new SecretKeySpec(KEY.getBytes(StandardCharsets.UTF_8), ALGORITHM);
             Cipher c = Cipher.getInstance(ALGORITHM);
             c.init(Cipher.ENCRYPT_MODE, key);
             byte[] encVal = c.doFinal(data.getBytes(StandardCharsets.UTF_8));
             return Base64.getEncoder().encodeToString(encVal);
-        } catch (Exception e) {
-            throw new Exception("Error while encrypting: " + e.toString());
-        }
-    }
-
-    public String decrypt(String encryptedData) throws Exception {
-        try {
-            Key key = new SecretKeySpec(KEY.getBytes(StandardCharsets.UTF_8), ALGORITHM);
-            Cipher c = Cipher.getInstance(ALGORITHM);
-            c.init(Cipher.DECRYPT_MODE, key);
-            byte[] decodedValue = Base64.getDecoder().decode(encryptedData.getBytes(StandardCharsets.UTF_8));
-            byte[] decValue = c.doFinal(decodedValue);
-            return new String(decValue, StandardCharsets.UTF_8);
-        } catch (Exception e) {
-            throw new Exception("Error while decrypting: " + e.toString());
+        } catch (NoSuchPaddingException | IllegalBlockSizeException | NoSuchAlgorithmException | BadPaddingException |
+                 InvalidKeyException e) {
+            throw new EncodingException(ResponseCode.PASSWORD_ENCRYPT_FAIL);
         }
     }
 
--- src/main/java/com/boj/guidance/util/api/ResponseCode.java
+++ src/main/java/com/boj/guidance/util/api/ResponseCode.java
@@ -14,6 +14,9 @@ public enum ResponseCode {
     USER_JOIN_FAIL(HttpStatus.UNAUTHORIZED, false, "회원가입 실패"),
     USER_LOGIN_FAIL(HttpStatus.UNAUTHORIZED, false, "로그인 실패"),
 
+    // 비밀번호 암호화 관련 오류
+    PASSWORD_ENCRYPT_FAIL(HttpStatus.CONFLICT, false, "암호화 실패"),
+
     // 성공
     USER_AUTH_SUCCESS(HttpStatus.ACCEPTED, true, "사용자 인증 성공"),
     USER_LOGIN_SUCCESS(HttpStatus.CREATED, true, "로그인 성공"),
