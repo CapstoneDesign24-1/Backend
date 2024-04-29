2723d8045a29446e9261175dbb569ad3599b3c27
zestbear <zestbear90@gmail.com>
2024-04-30 02:07:40 +0900

    fix: 코드 가독성 위한 toEntity 함수 사용

--- src/main/java/com/boj/guidance/controller/MemberController.java
+++ src/main/java/com/boj/guidance/controller/MemberController.java
@@ -1,6 +1,6 @@
 package com.boj.guidance.controller;
 
-import com.boj.guidance.config.PasswordEncoder;
+import com.boj.guidance.dto.MemberAuthRequestDto;
 import com.boj.guidance.dto.MemberJoinRequestDto;
 import com.boj.guidance.dto.MemberLoginRequestDto;
 import com.boj.guidance.dto.MemberResponseDto;
@@ -18,30 +18,15 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final MemberService memberService;
-    private final PasswordEncoder passwordEncoder;
 
     /**
      * 사용자 회원가입 기능
-     * @param dto
-     * @return
      */
     @PostMapping("/join")
-    public ApiResponse<MemberResponseDto> join(@RequestBody MemberJoinRequestDto dto) throws Exception {
+    public ApiResponse<MemberResponseDto> join(@RequestBody MemberJoinRequestDto dto) {
         log.info("회원가입 API 호출");
-        String encoded = passwordEncoder.encrypt(dto.getLogin_password());
-        MemberResponseDto joined = memberService.join(
-                MemberJoinRequestDto.builder()
-                        .handle(dto.getHandle())
-                        .login_id(dto.getLogin_id())
-                        .login_password(encoded)
-                        .bio(dto.getBio())
-                        .solved_count(dto.getSolved_count())
-                        .tier(dto.getTier())
-                        .rating(dto.getRating())
-                        .rating_by_problems_sum(dto.getRating_by_problems_sum())
-                        .rating_by_solved_count(dto.getRating_by_solved_count())
-                        .build()
-        );
+        MemberResponseDto joined = memberService.join(dto);
+
         return ApiResponse.success(ResponseCode.USER_JOIN_SUCCESS.getMessage(), joined);
     }
 
@@ -49,13 +34,10 @@ public class MemberController {
      * 사용자 로그인 기능
      */
     @PostMapping("/login")
-    public ApiResponse<MemberResponseDto> login(@RequestBody MemberLoginRequestDto dto) throws Exception {
+    public ApiResponse<MemberResponseDto> login(@RequestBody MemberLoginRequestDto dto) {
         log.info("로그인 API 호출");
-        String encoded = passwordEncoder.encrypt(dto.getLogin_password());
-        MemberResponseDto login = memberService.login(MemberLoginRequestDto.builder()
-                .login_id(dto.getLogin_id())
-                .login_password(encoded)
-                .build());
+        MemberResponseDto login = memberService.login(dto);
+
         return ApiResponse.success(ResponseCode.USER_LOGIN_SUCCESS.getMessage(), login);
     }
 
@@ -63,8 +45,9 @@ public class MemberController {
      * 백준 사용자 인증하기
      */
     @PostMapping("/auth")
-    public MemberResponseDto authorize() {
+    public MemberAuthRequestDto authorize() {
         log.info("인증 API 호출");
+
         return memberService.authorize();
     }
 
--- src/main/java/com/boj/guidance/dto/MemberJoinRequestDto.java
+++ src/main/java/com/boj/guidance/dto/MemberJoinRequestDto.java
@@ -1,43 +1,34 @@
 package com.boj.guidance.dto;
 
-import lombok.Builder;
+import com.boj.guidance.domain.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class MemberJoinRequestDto {
-    private String handle;              // 사용자명
-    private String login_id;            // 로그인 id
-    private String login_password;      // 로그인 pw
-    private String bio;                 // 자기소개
-    private Long solved_count;           // 푼 문제 수
-    private Long tier;                  // 티어 (1-31)
-    private Long rating;                // 레이팅
-    private Long rating_by_problems_sum;   // 푼 문제의 난이도 합으로 계산한 레이팅
-    private Long rating_by_solved_count;   // 푼 문제 수로 계산한 레이팅
+    private String handle;               // 사용자명
+    private String loginId;              // 로그인 id
+    private String loginPassword;        // 로그인 pw
+    private String bio;                  // 자기소개
+    private Long solvedCount;            // 푼 문제 수
+    private Long tier;                   // 티어 (1-31)
+    private Long rating;                 // 레이팅
+    private Long ratingByProblemsSum;    // 푼 문제의 난이도 합으로 계산한 레이팅
+    private Long ratingBySolvedCount;    // 푼 문제 수로 계산한 레이팅
 
-    @Builder
-    public MemberJoinRequestDto(
-            String handle,
-            String login_id,
-            String login_password,
-            String bio,
-            Long solved_count,
-            Long tier,
-            Long rating,
-            Long rating_by_problems_sum,
-            Long rating_by_solved_count
-    ) {
-        this.handle = handle;
-        this.login_id = login_id;
-        this.login_password = login_password;
-        this.bio = bio;
-        this.solved_count = solved_count;
-        this.tier = tier;
-        this.rating = rating;
-        this.rating_by_problems_sum = rating_by_problems_sum;
-        this.rating_by_solved_count = rating_by_solved_count;
+    public Member toEntity(String encoded) {
+        return Member.builder()
+                .handle(handle)
+                .loginId(loginId)
+                .loginPassword(encoded)
+                .bio(bio)
+                .solvedCount(solvedCount)
+                .tier(tier)
+                .rating(rating)
+                .ratingByProblemsSum(ratingByProblemsSum)
+                .ratingBySolvedCount(ratingBySolvedCount)
+                .build();
     }
 
 }
