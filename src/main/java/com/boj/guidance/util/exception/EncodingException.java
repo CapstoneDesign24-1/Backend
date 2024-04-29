dd1000b2f0d42ded384b5fe3009a168b0de17b4f
zestbear <zestbear90@gmail.com>
2024-04-30 02:05:22 +0900

    feature: 비밀번호 암호화 예외처리

--- src/main/java/com/boj/guidance/util/exception/EncodingException.java
+++ src/main/java/com/boj/guidance/util/exception/EncodingException.java
@@ -3,7 +3,6 @@ package com.boj.guidance.util.exception;
 import com.boj.guidance.util.api.ResponseCode;
 
 public class EncodingException extends BaseException {
-
     public EncodingException(ResponseCode code) {
         super(code);
     }
