f07386d4e8140e64155d64142a762c7dc1df0516
zestbear <zestbear90@gmail.com>
2024-05-03 02:46:24 +0900

    fix: 사용자 정보 반환에 id 추가

--- src/main/java/com/boj/guidance/dto/MemberResponseDto.java
+++ src/main/java/com/boj/guidance/dto/MemberResponseDto.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class MemberResponseDto {
+    private String id;                      // id
     private String handle;                  // 사용자명
     private String bio;                     // 자기소개
     private Long solvedCount;               // 푼 문제 수
@@ -19,6 +20,7 @@ public class MemberResponseDto {
 
     @Builder
     public MemberResponseDto(
+            String id,
             String handle,
             String bio,
             Long solvedCount,
@@ -27,6 +29,7 @@ public class MemberResponseDto {
             Long ratingByProblemsSum,
             Long ratingBySolvedCount
     ) {
+        this.id = id;
         this.handle = handle;
         this.bio = bio;
         this.solvedCount = solvedCount;
@@ -38,6 +41,7 @@ public class MemberResponseDto {
 
     public MemberResponseDto toResponse(Member entity) {
         return MemberResponseDto.builder()
+                .id(entity.getId())
                 .handle(entity.getHandle())
                 .bio(entity.getBio())
                 .solvedCount(entity.getSolvedCount())
