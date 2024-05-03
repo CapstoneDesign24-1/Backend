c5f0370aafe58668031bbd481b0d5f1c36fb60e8
zestbear <zestbear90@gmail.com>
2024-05-03 17:12:49 +0900

    fix: header, footer 간격 수정

--- src/main/java/com/boj/guidance/controller/page/PageController.java
+++ src/main/java/com/boj/guidance/controller/page/PageController.java
@@ -22,10 +22,49 @@ public class PageController {
     // 홈페이지 이동
     @GetMapping(value = {"/home", "/"})
     public String home(HttpSession session) {
+        String userName = (String) session.getAttribute("memberId");
+        return "home";
+    }
 
+    // 스터디 페이지
+    @GetMapping("/study")
+    public String study(HttpSession session) {
         String userName = (String) session.getAttribute("memberId");
         if (userName != null) {
-            return "home";
+            return "study";
+        } else {
+            return "redirect:/login";
+        }
+    }
+
+    // 문제 추천 페이지
+    @GetMapping("/recommendation")
+    public String recommendation(HttpSession session) {
+        String userName = (String) session.getAttribute("memberId");
+        if (userName != null) {
+            return "recommendation";
+        } else {
+            return "redirect:/login";
+        }
+    }
+
+    // 코드 분석 페이지
+    @GetMapping("/codeAnalysis")
+    public String codeAnalysis(HttpSession session) {
+        String userName = (String) session.getAttribute("memberId");
+        if (userName != null) {
+            return "codeAnalysis";
+        } else {
+            return "redirect:/login";
+        }
+    }
+
+    // 커뮤니티 페이지
+    @GetMapping("/community")
+    public String community(HttpSession session) {
+        String userName = (String) session.getAttribute("memberId");
+        if (userName != null) {
+            return "community";
         } else {
             return "redirect:/login";
         }
