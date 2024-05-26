f49e0042a0503f3784358c56a50ef94b23e6c975
zestbear <zestbear90@gmail.com>
2024-05-27 00:43:15 +0900

    feature: 에러 핸들링

--- src/main/java/com/boj/guidance/util/api/ResponseCode.java
+++ src/main/java/com/boj/guidance/util/api/ResponseCode.java
@@ -14,10 +14,11 @@ public enum ResponseCode {
     REDIS_LOCK_FAIL(HttpStatus.LOCKED, false, "Redis Lock 실패"),
 
     // 사용자 관련 오류
-    USER_AUTH_FAIL(HttpStatus.UNAUTHORIZED, false, "사용자 인증 실패"),
-    USER_JOIN_FAIL(HttpStatus.UNAUTHORIZED, false, "회원가입 실패"),
-    USER_LOGIN_FAIL(HttpStatus.UNAUTHORIZED, false, "로그인 실패"),
+    USER_AUTH_FAIL(HttpStatus.BAD_REQUEST, false, "사용자 인증 실패"),
+    USER_JOIN_FAIL(HttpStatus.BAD_REQUEST, false, "회원가입 실패"),
+    USER_LOGIN_FAIL(HttpStatus.BAD_REQUEST, false, "로그인 실패"),
     USER_ROLE_CHANGE_FAIL(HttpStatus.BAD_REQUEST, false, "Role 변경 실패"),
+    USER_STATE_CHANGE_FAIL(HttpStatus.BAD_REQUEST, false, "State 변경 실패"),
     USER_NOT_EXIST(HttpStatus.BAD_REQUEST, false, "사용자 정보 없음"),
 
     // 비밀번호 암호화 관련 오류
@@ -30,13 +31,17 @@ public enum ResponseCode {
     // Django 서버 관련 오류
     ANALYSIS_IMAGE_FAIL(HttpStatus.BAD_REQUEST, false, "분석 이미지 오류"),
 
+    // 스터디그룹 관련 오류
+    STUDY_GROUP_NOT_EXIST(HttpStatus.BAD_REQUEST, false, "스터디그룹 정보 없음"),
+
     // 성공
-    USER_AUTH_SUCCESS(HttpStatus.ACCEPTED, true, "사용자 인증 성공"),
-    USER_LOGIN_SUCCESS(HttpStatus.CREATED, true, "로그인 성공"),
+    USER_AUTH_SUCCESS(HttpStatus.OK, true, "사용자 인증 성공"),
+    USER_LOGIN_SUCCESS(HttpStatus.OK, true, "로그인 성공"),
     USER_JOIN_SUCCESS(HttpStatus.CREATED, true, "회원가입 성공"),
-    USER_ROLE_CHANGE_SUCCESS(HttpStatus.ACCEPTED, true, "Role 변경 성공"),
-    ALGORITHM_NAME_SEARCH_SUCCESS(HttpStatus.ACCEPTED, true, "문제 검색 성공"),
-    PROBLEM_RECOMMEND_SUCCESS(HttpStatus.ACCEPTED, true, "문제 추천 성공");
+    USER_ROLE_CHANGE_SUCCESS(HttpStatus.OK, true, "Role 변경 성공"),
+    USER_STATE_CHANGE_SUCCESS(HttpStatus.OK, true, "State 변경 성공"),
+    ALGORITHM_NAME_SEARCH_SUCCESS(HttpStatus.FOUND, true, "문제 검색 성공"),
+    PROBLEM_RECOMMEND_SUCCESS(HttpStatus.FOUND, true, "문제 추천 성공");
 
     private final HttpStatus httpStatus;
     private final Boolean success;
