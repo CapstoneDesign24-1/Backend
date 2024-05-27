8eb6805b26471540df1080297cbe14a0d5f30ab4
zestbear <zestbear90@gmail.com>
2024-05-28 02:50:09 +0900

    fix: 오타 수정

--- src/main/java/com/boj/guidance/repository/StudyGroupRepository.java
+++ src/main/java/com/boj/guidance/repository/StudyGroupRepository.java
@@ -1,18 +1,15 @@
 package com.boj.guidance.repository;
 
-import com.boj.guidance.domain.Member;
 import com.boj.guidance.domain.StudyGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface StudyGroupRepository extends JpaRepository<StudyGroup, String> {
-    List<StudyGroup> findAllByDeletedIsFalse();
+    List<StudyGroup> findAllByIsDeletedIsFalse();
 
     // TODO: 쿼리 완성
-    @Query("SELECT * FROM Member WHERE ")
-    List<Member> findMemberByWeakAlgorithm_AAndDeletedIsFalse();
+//    List<Member> findMemberByWeakAlgorithm_AAndDeletedIsFalse();
 }
