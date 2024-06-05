83798b4e9691cea757045a456bfe8ce9f22db374
zestbear <zestbear90@gmail.com>
2024-06-05 16:37:53 +0900

    hotfix: cors 보안 설정 추가 및 에러 핸들링

--- src/main/java/com/boj/guidance/BOJGuidanceApplication.java
+++ src/main/java/com/boj/guidance/BOJGuidanceApplication.java
@@ -3,8 +3,11 @@ package com.boj.guidance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableCaching
 @SpringBootApplication
@@ -14,6 +17,19 @@ public class BOJGuidanceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BOJGuidanceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://bojguidance.com", "http://www.bojguidance.com")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 }
